cc4b4e1fa2310315b4319fa6d5c3a21dec28b87d
phuijsmans <huijsmanspieter@gmail.com>
2024-05-07 16:55:14 +0200

    Changed/fixed how portrait gets name image file

--- src/components/ShowChampionsList.jsx
+++ src/components/ShowChampionsList.jsx
@@ -6,11 +6,21 @@ import { ShowChampionDetails } from "./ShowChampionDetails";
 
 export const ShowChampionsList = ({ allChampions, freeChampionsRotation }) => {
   // const { champions } = useContext(ChampionsContext);
-  const championsList = Object.values(allChampions);
+  const originalChampionsList = Object.values(allChampions);
+  const [championsList, setChampionsList] = useState(originalChampionsList);
   const [selectedChampion, setSelectedChampion] = useState(null);
   const test = (champion) => {
     setSelectedChampion(champion);
   };
+
+  const adjustChampionList = (championsList) => {
+    let clonedChampionsList = JSON.parse(JSON.stringify(championsList));
+    let imageName = "";
+    // clonedChampionsList[0].newName = championsList[0].name;
+    console.log(clonedChampionsList[0]);
+  };
+
+  adjustChampionList(originalChampionsList);
 
   return (
     <div>
--- src/components/ShowChampionsPortraitList.jsx
+++ src/components/ShowChampionsPortraitList.jsx
@@ -1,35 +1,9 @@
 import "../css/PortraitStyle.css";
-import { useState } from "react";
 
 export const ShowChampionsPortraitList = ({ champion, onClick }) => {
-  const [portraitName, setPortraitName] = useState(
-    champion.name.replace(" ", "").replace("'", "")
-  );
-
-  // check to see how many capital letters a string has.
-  // const showNumberOfCapitalLetters = (portraitName.match(/[A-Z]/g) || []).length;
-
-  const makeSingleCapital = ["KaiSa", "LeBlanc", "VelKoz", "ChoGath", "KhaZix"];
-
-  if (portraitName === "Wukong") {
-    setPortraitName("MonkeyKing");
-  } else if (portraitName === "Nunu& Willump") {
-    setPortraitName("Nunu");
-  } else if (portraitName === "Dr.Mundo") {
-    setPortraitName("DrMundo");
-  } else if (portraitName === "RenataGlasc") {
-    setPortraitName("Renata");
-  } else if (
-    portraitName === makeSingleCapital[0] ||
-    portraitName === makeSingleCapital[1] ||
-    portraitName === makeSingleCapital[2] ||
-    portraitName === makeSingleCapital[3] ||
-    portraitName === makeSingleCapital[4]
-  ) {
-    setPortraitName(portraitName[0] + portraitName.slice(1).toLowerCase());
-  }
-
+  const portraitName = champion.id;
   const portraitURL = `https://ddragon.leagueoflegends.com/cdn/14.9.1/img/champion/${portraitName}.png`;
+
   const test = () => {
     console.log(champion.name);
   };
